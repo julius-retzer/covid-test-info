421ce0b11ee7bb3ff34d84bf663e8678a4f9e58b
tomasko910 <tomas.p.kolarcik@gmail.com>
2020-04-04 16:46:41 +0200

    add 'add_results' view (mock data only)

--- covid_test_backend/api/models.py
+++ covid_test_backend/api/models.py
@@ -8,6 +8,6 @@ from django.db import models
 
 class TestResult(models.Model):
     executed_at = models.DateField('day month and year the test was executed')
-    hash = models.BinaryField(60)
+    hash_patient = models.CharField(max_length=60)
     result = models.BooleanField()
 
--- covid_test_backend/api/views.py
+++ covid_test_backend/api/views.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 import random
 
-from django.http import JsonResponse, HttpResponseNotFound
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponse
 
+from .models import TestResult
 
 def check(request):
     assert request.method == 'POST'
@@ -24,3 +25,29 @@ def check(request):
     return JsonResponse({
         'test_result': random.random() < 0.5
     })
+
+
+
+def add_results(request):
+    # flow: rozdel input na riadky, kazdy riadok na jednotlive kusy dat, s nimi vyrob instanciu TestResult, uloz do DB.
+
+    # POZOR: date format musi byt YYYY-MM-DD
+    mock = """2020-01-01;$1$qqqqqq=$;0
+2020-01-01;$1$wwwwwww=$;0
+2020-01-01;$1$eeeeee=$;0
+2020-01-01;$rrrrr=$;0
+2020-01-01;$1$nJuP$LkmznbaSd!3;1"""
+
+    # lines = request.POST.get('test_results').split('\n')
+    lines = mock.split('\n')
+
+    for line in lines:
+        fields = line.split(';')
+        new_test_result = TestResult(
+            executed_at=fields[0],
+            hash_patient=fields[1],
+            result=fields[2]
+        )
+        new_test_result.save()
+
+    return HttpResponse(f'pridali sme {len(lines)} výsledkov testov')
